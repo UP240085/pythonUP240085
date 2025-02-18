3e9ecb7d81aaf1cd6ee06ddfc392c28a1b38941f
UP240085 <up240085@alumnos.upa.edu.mx>
2025-02-18 08:26:01 -0600

    .

--- Ejercicio 1.py
+++ Ejercicio 1.py
@@ -1,1 +1,7 @@
 #Día 3 
+#Área tríangulo
+base = float(input("Ingresa el valor de la base: "))
+altura = float(input("Ingresa la altura: ")) 
+área = 0.5 * base * altura
+print("El área del triángulo es:",área)
+5
