dffeac6fe7a25ffea79908482ecd4e15ab8f2e1c
UP240085 <up240085@alumnos.upa.edu.mx>
2025-03-12 09:37:38 -0600

    final día 6

--- Día6.py
+++ Día6.py
@@ -39,7 +39,39 @@ parents = family_members[4:]
 print(f"Mis padres son {parents}")
 
 #2 Create fruits, vegetables and animal products tuples. Join the three tuples and assign it to a variable called food_stuff_tp.
-fruits = tuple("apple", "mango")
-vegetables = tuple("carrot", "onion")
-animal_products = tuple("egg", "milk")
+print("2- Create fruits, vegetables and animal products tuples. Join the three tuples and assign it to a variable called food_stuff_tp.")
+fruits = ("apple", "mango", "pear")
+vegetables = ("carrot", "onion")
+animal_products = ("egg", "milk")
 food_stuff_tp = fruits + vegetables + animal_products
+print(food_stuff_tp)
+
+#3 Change the about food_stuff_tp tuple to a food_stuff_lt list
+print("3- Change the about food_stuff_tp tuple to a food_stuff_lt list")
+food_stuff_lt = list(food_stuff_tp)
+print(food_stuff_lt)
+
+#4 Slice out the middle item or items from the food_stuff_tp tuple or food_stuff_lt list.
+print("4- Slice out the middle item or items from the food_stuff_tp tuple or food_stuff_lt list.")
+print(len(food_stuff_lt))
+middle_item = food_stuff_lt[(len(food_stuff_lt)//2)]
+print (middle_item)
+
+#5 Slice out the first three items and the last three items from food_staff_lt list
+print("5- Slice out the first three items and the last three items from food_staff_lt list")
+first_three = food_stuff_lt[:3]
+print(first_three)
+last_three = food_stuff_lt[-3:]
+print(last_three)
+
+#6 Delete the food_staff_tp tuple completely
+print("6- Delete the food_staff_tp tuple completely")
+del food_stuff_tp
+
+#7 Check if an item exists in tuple:
+print("7- Check if an item exists in tuple:")
+nordic_countries = ('Denmark', 'Finland','Iceland', 'Norway', 'Sweden')
+print(f"Existe Estonia en nordic_countries: {"Estonia" in nordic_countries}")
+print(f"Existe Iceland en nordic_countries: {"Iceland" in nordic_countries}")
+
+
