a243a9e01388fb51918fb0584fac8c5beb3f3b46
UP240085 <up240085@alumnos.upa.edu.mx>
2025-03-13 11:09:38 -0600

    ,

---
+++ Día7.py
@@ -0,0 +1,20 @@
+# sets
+it_companies = {'Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon'}
+A = {19, 22, 24, 20, 25, 26}
+B = {19, 22, 20, 25, 26, 24, 28, 27}
+age = [22, 19, 24, 25, 26, 24, 25, 24]
+
+#Exercises: Level 1
+
+#1- Find the length of the set it_companies
+print("1- Find the length of the set it_companies")
+
+#2- Add 'Twitter' to it_companies
+print("2- Add 'Twitter' to it_companies")
+
+#3- Insert multiple IT companies at once to the set it_companies
+print("3- Insert multiple IT companies at once to the set it_companies")
+
+#4- Remove one of the companies from the set it_companies
+
+#5- What is the difference between remove and discard
