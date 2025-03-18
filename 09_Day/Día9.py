3ec84db275c6883a0ab468d316bc86d06deb8fe4
UP240085 <up240085@alumnos.upa.edu.mx>
2025-03-18 09:23:42 -0600

    9

--- 09_Day/Día9.py
+++ 09_Day/Día9.py
@@ -8,3 +8,14 @@ if age >= 18:
     print("You are old enough to drive")
 else:
     print(f"Wait for {18 - age} years")
+
+#2- Compare the values of my_age and your_age using if … else. Who is older (me or you)? Use input(“Enter your age: ”) to get the age as input. You can use a nested condition to print 'year' for 1 year difference in age, 'years' for bigger differences, and a custom text if my_age = your_age. 
+print("2- Compare the values of my_age and your_age using if … else. Who is older (me or you)? Use input(“Enter your age: ”) to get the age as input. You can use a nested condition to print 'year' for 1 year difference in age, 'years' for bigger differences, and a custom text if my_age = your_age.")
+my_age = 18
+your_age = int(input("Enter your age: "))
+if my_age > your_age:
+    print(f"I am {my_age - your_age} years older than you")
+elif my_age < your_age:
+    print(f"You are {your_age - my_age} years older than me")
+else:
+    print("We are the same age")
